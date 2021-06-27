c2e097b2d97b4c72872441f6c3a66912fb552093
Dmitriy Polyakov <disla2000@mail.ru>
2021-06-27 05:28:54 +0500

    edit token

--- image_processing.py
+++ image_processing.py
@@ -1,7 +1,7 @@
 from model_cyclegan import CycleGAN
 from model_nst import StyleTransfer
 from aiogram import Bot
-from config import API_TOKEN
+from config import BOT_API_TOKEN
 import keyboards as kb
 import warnings
 warnings.filterwarnings("ignore")
@@ -15,7 +15,7 @@ async def cycle_gan(message, image, type_algo):
 
     new_image = CycleGAN.run_gan(wts_path, image)
 
-    tmp_bot = Bot(token=API_TOKEN)
+    tmp_bot = Bot(token=BOT_API_TOKEN)
     await tmp_bot.send_photo(message.chat.id, photo=new_image)
     await tmp_bot.send_message(message.chat.id, "Надеюсь, тебе понравилось.\n\n Хочешь попробовать еще раз?",
                                reply_markup=kb.algo_keyboard())
@@ -25,7 +25,7 @@ async def cycle_gan(message, image, type_algo):
 async def style_transfer(message, style_image, content_image):
     new_image = StyleTransfer.run_nst(style_image, content_image)
 
-    tmp_bot = Bot(token=API_TOKEN)
+    tmp_bot = Bot(token=BOT_API_TOKEN)
     await tmp_bot.send_photo(message.chat.id, photo=new_image)
     await tmp_bot.send_message(message.chat.id, "Надеюсь, тебе понравилось.\n\n Хочешь попробовать еще раз?",
                                reply_markup=kb.algo_keyboard())
